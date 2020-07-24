9f79c92a4c9601be24fd58407b9abaf42c84f327
Dmitry Plevkov <wwarne@gmail.com>
2020-07-24 23:49:23 +0300

    Some type annotations

--- adapters/html_tools.py
+++ adapters/html_tools.py
@@ -1,3 +1,7 @@
+from typing import Sequence
+
+from bs4.element import Tag
+
 DEFAULT_BLACKLIST_TAGS = (
     'script',
     'time'
@@ -13,7 +17,8 @@ DEFAULT_UNWRAPLIST_TAGS = (
     'footer'
 )
 
-def remove_buzz_attrs(soup):
+
+def remove_buzz_attrs(soup: Tag) -> Tag:
     """Remove all attributes except some special tags."""
     for tag in soup.find_all(True):
         if tag.name == 'a':
@@ -29,7 +34,11 @@ def remove_buzz_attrs(soup):
 
     return soup
 
-def remove_buzz_tags(soup, blacklist=DEFAULT_BLACKLIST_TAGS, unwraplist=DEFAULT_UNWRAPLIST_TAGS):
+
+def remove_buzz_tags(soup: Tag,
+                     blacklist: Sequence[str] = DEFAULT_BLACKLIST_TAGS,
+                     unwraplist: Sequence[str] = DEFAULT_UNWRAPLIST_TAGS
+                     ) -> None:
     """Remove most of tags, leaves only tags significant for text analysis."""
     for tag in soup.find_all(True):
         if tag.name in blacklist:
@@ -37,7 +46,8 @@ def remove_buzz_tags(soup, blacklist=DEFAULT_BLACKLIST_TAGS, unwraplist=DEFAULT_
         elif tag.name in unwraplist:
             tag.unwrap()
 
-def remove_all_tags(soup):
+
+def remove_all_tags(soup: Tag) -> None:
     """Unwrap all tags."""
     for tag in soup.find_all(True):
         tag.unwrap()
--- adapters/inosmi_ru.py
+++ adapters/inosmi_ru.py
@@ -1,12 +1,10 @@
 from bs4 import BeautifulSoup
-import requests
-import pytest
 
 from .exceptions import ArticleNotFound
 from .html_tools import remove_buzz_attrs, remove_buzz_tags, remove_all_tags
 
 
-def sanitize(html, plaintext=False):
+def sanitize(html: str, plaintext: bool = False) -> str:
     soup = BeautifulSoup(html, 'html.parser')
     articles = soup.select("article.article")
 
@@ -33,35 +31,3 @@ def sanitize(html, plaintext=False):
         remove_all_tags(article)
         text = article.get_text()
     return text.strip()
-
-
-def test_sanitize():
-    resp = requests.get('https://inosmi.ru/economic/20190629/245384784.html')
-    resp.raise_for_status()
-    clean_text = sanitize(resp.text)
-
-    assert clean_text.startswith('<article>')
-    assert clean_text.endswith('</article>')
-    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
-    assert 'За несколько часов до встречи с Си' in clean_text
-
-    assert '<img src="' in clean_text
-    assert '<a href="' in clean_text
-    assert '<h1>' in clean_text
-
-    clean_plaintext = sanitize(resp.text, plaintext=True)
-
-    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_plaintext
-    assert 'За несколько часов до встречи с Си' in clean_plaintext
-
-    assert '<img src="' not in clean_plaintext
-    assert '<a href="' not in clean_plaintext
-    assert '<h1>' not in clean_plaintext
-    assert '</article>' not in clean_plaintext
-    assert '<h1>' not in clean_plaintext
-
-def test_sanitize_wrong_url():
-    resp = requests.get('http://example.com')
-    resp.raise_for_status()
-    with pytest.raises(ArticleNotFound):
-        sanitize(resp.text)
